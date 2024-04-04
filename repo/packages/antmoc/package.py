806c2288be5c4e95dbd026ae9ea20b3589a6b2b6
one <wangan.cs@gmail.com>
2024-04-05 00:52:13 +0800

    Update antmoc package

--- repo/packages/antmoc/package.py
+++ repo/packages/antmoc/package.py
@@ -1,4 +1,4 @@
-from spack import *
+from spack.package import *
 
 
 class Antmoc(BundlePackage):
@@ -8,15 +8,17 @@ class Antmoc(BundlePackage):
 
     maintainers = ['alephpiece']
 
+    license("MIT")
+
     version('0.1.15')
 
     variant('mpi', default=False, description='Enable MPI support')
 
-    depends_on('cmake@3.16', type='build')
+    depends_on('cmake@3.16:', type='build')
     depends_on('mpi@3.0:3.1', when='+mpi', type=('build', 'link', 'run'))
-    depends_on('cxxopts@=3.0.0')
+    depends_on('cxxopts@=3.0')
     depends_on('fmt@6.0.0:8.0.0+shared')
-    depends_on('tinyxml2@7.0.0:8.0.0')
+    depends_on('tinyxml2@7.0:9.0+shared')
     depends_on('toml11@3.6:3.7')
     depends_on('hdf5@=1.10.8~mpi', when='~mpi')
     depends_on('hdf5@=1.10.8+mpi', when='+mpi')
