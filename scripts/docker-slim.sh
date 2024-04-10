8e6ab84a7f932ac9c1ab7b45e167e066d2318df0
one <wangan.cs@gmail.com>
2024-04-10 18:59:22 +0800

    Add tag to slimmed image

--- scripts/docker-slim.sh
+++ scripts/docker-slim.sh
@@ -1,8 +1,9 @@
 # See https://github.com/slimtoolkit/slim
 
 IMAGE=${1:-"antmoc/antmoc-ci:0.1.16-alpha"}
+NEWTAG=${IMAGE%-*}
 
-# /opt is the largest directory
+/opt is the largest directory
 slim build --target $IMAGE \
   --mount $(pwd)/ant-moc:/opt/mnt/ant-moc \
   --mount $(pwd)/scripts:/opt/mnt/scripts \
@@ -18,4 +19,5 @@ slim build --target $IMAGE \
   --exclude-pattern /usr/lib/x86_64-linux-gnu/dri \
   --preserve-path /tmp \
   --path-perms /usr/bin/sudo:4755 \
+  --tag $NEWTAG \
   --exec-file ./scripts/test-antmoc.sh
