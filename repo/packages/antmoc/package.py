f6c14fbc1d65b7c319a4ac95d610a113b17563a9
one <wangan.cs@gmail.com>
2024-04-12 20:01:50 +0800

    Update package

--- repo/packages/antmoc/package.py
+++ repo/packages/antmoc/package.py
@@ -19,7 +19,7 @@ class Antmoc(BundlePackage):
     depends_on('cmake@3.16:', type='build')
     depends_on('mpi@3', when='+mpi', type=('build', 'link', 'run'))
     depends_on('cxxopts@3')
-    depends_on('fmt@6 +shared', when='@:0.1.15')
+    depends_on('fmt@6:8 +shared', when='@:0.1.15')
     depends_on('fmt@8:10 +shared', when='@0.1.16:')
     depends_on('tinyxml2@7:10 +shared')
     depends_on('toml11@3.6:3.7')
